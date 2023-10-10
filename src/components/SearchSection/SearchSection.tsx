7a51b8b2558493de41573d7f0437f943fe59ab6b
Soroush Sharifi <sharifysoroush@gmail.com>
2023-10-10 15:13:11 +0330

    create-search-box

--- src/components/SearchSection/SearchSection.tsx
+++ src/components/SearchSection/SearchSection.tsx
@@ -1,25 +1,44 @@
-import { ChangeEvent, useState } from "react";
+import { ChangeEvent, useEffect, useState } from "react";
+import { optionType } from "../../types";
 
 export default function SearchSection(): JSX.Element {
   const [term, setTerm] = useState<string>("");
+  const [options, setOptions] = useState<[]>([]);
+  const [city, setCity] = useState<optionType | null>(null);
 
   const APIKey = import.meta.env.VITE_REACT_API_KEY;
+
+  const onInputChange = (e: ChangeEvent<HTMLInputElement>) => {
+    const value = e.target.value.trim();
+    setTerm(value);
+    if (value === "") return setOptions([]);
+    getSearchOptions(value);
+  };
 
   const getSearchOptions = (value: string) => {
     fetch(
       `http://api.openweathermap.org/geo/1.0/direct?q=${value.trim()}&limit=5&appid=${APIKey}`
     )
       .then((res) => res.json())
+      .then((data) => setOptions(data));
+  };
+
+  const onOptionSelect = (option: optionType) => {
+    setCity(option);
+    fetch(
+      `https://api.openweathermap.org/data/2.5/weather?lat=${option.lat}&lon=${option.lon}&units=metric&appid=${APIKey}`
+    )
+      .then((res) => res.json())
       .then((data) => console.log({ data }));
   };
 
-  const onInputChange = (e: ChangeEvent<HTMLInputElement>) => {
-    const value = e.target.value.trim();
-    setTerm(value);
-    if (value === "") return;
-    //test comment 2
-    getSearchOptions(value);
-  };
+  useEffect(() => {
+    if (city) {
+      setTerm(city.name);
+      setOptions([]);
+    }
+  }, [city]);
+
   return (
     <section
       className=" basis-full sm:basis-11/12 h-full rounded bg-slate-800 flex 
@@ -32,14 +51,26 @@ export default function SearchSection(): JSX.Element {
         Enter below a place you want to know the weather of and select an option
         from the dropdown
       </p>
-      <div className="flex mt-10 sm:mt-6">
+      <div className="relative flex mt-10 sm:mt-6">
         <input
           type="text"
           value={term}
           onChange={onInputChange}
           className="px-2 py-1 rounded-l-md bg-slate-200 outline-none text-slate-900"
         />
-        <button className="rounded-r-md border-2 border-slate-200 px-2 py-1 cursor-pointer hover:bg-slate-600 transition">
+        <ul className="absolute top-9 bg-slate-200 ml-1 text-slate-800 rounded-b-md">
+          {options.map((opt: optionType, index: number) => (
+            <li key={index}>
+              <button
+                className="text-left text-sm w-full hover:bg-slate-900 hover:text-slate-200 px-2 py-1"
+                onClick={() => onOptionSelect(opt)}
+              >
+                {opt.name}, {opt.country}
+              </button>
+            </li>
+          ))}
+        </ul>
+        <button className="rounded-r-md border-2 border-slate-200 px-2 py-1 hover:bg-slate-600 transition">
           Search
         </button>
       </div>
